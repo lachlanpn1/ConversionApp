2fb53bfa11b833aed0dcd1391da12809ef8d31b7
Lachlan <101626915@student.swin.edu.au>
2018-08-31 12:12:57 +1000

    Add intents to MainActivity

--- app/src/main/java/com/example/lachlan/conversionapp/MainActivity.java
+++ app/src/main/java/com/example/lachlan/conversionapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lachlan.conversionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,20 +8,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button_distanceConverter;
-    private Button button_tempConverter;
+    private enum Converter {
+        Temperature, Distance
+    }
 
     private View.OnClickListener distanceConverterOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            showConverter(Converter.Distance);
         }
     };
 
     private View.OnClickListener tempConverterOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            showConverter(Converter.Temperature);
         }
     };
     
@@ -32,11 +34,28 @@ public class MainActivity extends AppCompatActivity {
     }
     
     private void initialiseUI() {
+        Button button_distanceConverter;
+        Button button_tempConverter;
+
         button_distanceConverter = findViewById(R.id.Button_distanceConverter);
         button_tempConverter = findViewById(R.id.Button_temperatureConverter);
         
         button_distanceConverter.setOnClickListener(distanceConverterOnClickListener);
         button_tempConverter.setOnClickListener(tempConverterOnClickListener);
+    }
+
+    private void showConverter(Converter converter)
+    {
+        switch(converter)
+        {
+            case Temperature:
+                Intent tempConverterIntent = new Intent(this, TemperatureActivity.class);
+                startActivity(tempConverterIntent);
+                break;
+            case Distance:
+                Intent distConverterIntent = new Intent(this, DistanceActivity.class);
+                startActivity(distConverterIntent);
+        }
     }
     
 }
