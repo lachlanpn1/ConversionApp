c69c3304e68e3c759812135d6418fb4821c7e67c
Lachlan <101626915@student.swin.edu.au>
2018-09-07 11:45:22 +1000

    add landscape variation for temperature activity

--- app/src/main/java/com/example/lachlan/conversionapp/DistanceActivity.java
+++ app/src/main/java/com/example/lachlan/conversionapp/DistanceActivity.java
@@ -2,6 +2,13 @@ package com.example.lachlan.conversionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class DistanceActivity extends AppCompatActivity {
 
@@ -9,5 +16,163 @@ public class DistanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
+        initialiseUI(savedInstanceState);
+    }
+
+    private static final double INCHES_TO_CM = 2.54;
+    private static final double FEET_TO_CM = 30.48;
+    private static final double MILES_TO_CM = 160934.0;
+
+    private static final double INCHES_TO_M = 0.0254;
+    private static final double FEET_TO_M = 0.3048;
+    private static final double MILES_TO_M = 1609.34;
+
+    private TextView inchesResult, feetResult, milesResult;
+
+    private EditText inchesInput, feetInput, milesInput;
+
+    private CheckBox metresCheckBox;
+
+    DecimalFormat df = new DecimalFormat("###.###");
+
+    private void initialiseUI(Bundle savedInstanceState) {
+        Button btnConvert = findViewById(R.id.convert_button);
+        btnConvert.setOnClickListener(btnConvertListener);
+
+        inchesResult = findViewById(R.id.inchesResult_textView);
+        feetResult = findViewById(R.id.feetResult_textView);
+        milesResult = findViewById(R.id.milesResult_textView);
+
+        inchesInput = findViewById(R.id.inches_editText);
+        feetInput = findViewById(R.id.feet_editText);
+        milesInput = findViewById(R.id.miles_editText);
+
+        metresCheckBox = findViewById(R.id.metres_chkBox);
+
+        restoreState(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        String[] results = new String[3];
+        if (!inchesResult.getText().toString().equals(""))
+            results[0] = inchesResult.getText().toString();
+        else
+            results[0] = "";
+
+        if(!feetResult.getText().toString().equals(""))
+            results[1] = feetResult.getText().toString();
+        else
+            results[1] = "";
+
+        if(!milesResult.getText().toString().equals(""))
+            results[2] = milesResult.getText().toString();
+        else
+            results[2] = "";
+
+        String[] inputs = new String[3];
+        inputs[0] = inchesInput.getText().toString();
+        inputs[1] = feetInput.getText().toString();
+        inputs[2] = milesInput.getText().toString();
+
+        outState.putStringArray("RESULTS",results);
+        outState.putStringArray("INPUTS",inputs);
+        outState.putBoolean("METRES",metresCheckBox.isChecked());
+
+        super.onSaveInstanceState(outState);
+    }
+
+    private void restoreState(Bundle inState)
+    {
+        if (inState == null) { return; }
+        String[] results = inState.getStringArray("RESULTS");
+        String[] inputs = inState.getStringArray("INPUTS");
+        boolean metres = inState.getBoolean("METRES");
+
+        inchesResult.setText(results[0]);
+        feetResult.setText(results[1]);
+        milesResult.setText(results[2]);
+
+        inchesInput.setText(inputs[0]);
+        feetInput.setText(inputs[1]);
+        milesInput.setText(inputs[2]);
+
+        metresCheckBox.setChecked(metres);
+
+
+
+    }
+
+    private View.OnClickListener btnConvertListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if(metresCheckBox.isChecked())
+            {
+                convertToMetres();
+            } else {
+                convertToCentimetres();
+            }
+        }
+    };
+
+    private void convertToCentimetres()
+    {
+        if(!(inchesInput.getText().toString().equals(""))) {
+            String inputString = inchesInput.getText().toString();
+            double inputNum = Double.parseDouble(inputString);
+
+            String result = "= " + df.format(inputNum * INCHES_TO_CM) + " cm";
+
+            inchesResult.setText(result);
+        }
+
+        if(!(feetInput.getText().toString().equals(""))) {
+            String inputString = feetInput.getText().toString();
+            double inputNum = Double.parseDouble(inputString);
+
+            String result = "= " + df.format(inputNum * FEET_TO_CM) + " cm";
+
+            feetResult.setText(result);
+        }
+
+        if(!(milesInput.getText().toString().equals(""))) {
+            String inputString = milesInput.getText().toString();
+            double inputNum = Double.parseDouble(inputString);
+
+            String result = "= " + df.format(inputNum * MILES_TO_CM) + " cm";
+            // String result = Double.toString(inputNum * MILES_TO_CM);
+
+            milesResult.setText(result);
+        }
+    }
+
+    private void convertToMetres()
+    {
+        if(!(inchesInput.getText().toString().equals(""))) {
+            String inputString = inchesInput.getText().toString();
+            double inputNum = Double.parseDouble(inputString);
+
+            String result = "= " + df.format(inputNum * INCHES_TO_M) + " m";
+
+            inchesResult.setText(result);
+        }
+
+        if(!(feetInput.getText().toString().equals(""))) {
+            String inputString = feetInput.getText().toString();
+            double inputNum = Double.parseDouble(inputString);
+
+            String result = "= " + df.format(inputNum * FEET_TO_M) + " m";
+
+            feetResult.setText(result);
+        }
+
+        if(!(milesInput.getText().toString().equals(""))) {
+            String inputString = milesInput.getText().toString();
+            double inputNum = Double.parseDouble(inputString);
+
+            String result = "= " + df.format(inputNum * MILES_TO_M) + " m";
+
+            milesResult.setText(result);
+        }
     }
 }
--- app/src/main/java/com/example/lachlan/conversionapp/TemperatureActivity.java
+++ app/src/main/java/com/example/lachlan/conversionapp/TemperatureActivity.java
@@ -2,12 +2,177 @@ package com.example.lachlan.conversionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class TemperatureActivity extends AppCompatActivity {
+
+    private TextView outputTextView;
+    private EditText inputEditText;
+    private Spinner inputSpinner, outputSpinner;
+
+    private enum Unit {
+        CELSIUS, FAHRENHEIT, KELVIN
+    }gx43
+
+    DecimalFormat df = new DecimalFormat("###.###");
+
+    TextWatcher tw = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                convertTemp();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
+        initialiseUI(savedInstanceState);
+    }
+
+    private void initialiseUI(Bundle savedInstanceState)
+    {
+        inputEditText = findViewById(R.id.input_editText);
+        outputTextView = findViewById(R.id.output_textView);
+        inputSpinner = findViewById(R.id.input_spinner);
+        outputSpinner = findViewById(R.id.output_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.temperature_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        inputSpinner.setAdapter(adapter);
+        outputSpinner.setAdapter(adapter);
+
+        inputEditText.addTextChangedListener(tw);
+
+        inputSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                convertTemp();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        outputSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                convertTemp();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void convertTemp()
+    {
+        double input;
+        Unit inputUnit = getUnit(inputSpinner);
+        Unit outputUnit = getUnit(outputSpinner);
+
+        String inputString = inputEditText.getText().toString();
+        if (!inputString.equals(""))
+        {
+            input = Double.parseDouble(inputString);
+        } else {
+            input = 0.0;
+        }
+
+
+        double result;
+
+        switch(outputUnit)
+        {
+            case CELSIUS:
+                result = toCelsius(input,inputUnit);
+                break;
+            case FAHRENHEIT:
+                result = toFahrenheit(input,inputUnit);
+                break;
+            default:
+                result = toKelvin(input,inputUnit);
+                break;
+        }
+
+        String outputString = df.format(result);
+        outputTextView.setText(outputString);
+    }
+
+    private double toCelsius(double temp, Unit unit)
+    {
+        switch(unit)
+        {
+            case FAHRENHEIT:
+                return (temp - 32) * (5 / 9);
+            case KELVIN:
+                return (temp - 273.15);
+            default:
+                return temp;
+        }
+    }
+
+    private double toFahrenheit(double temp, Unit unit)
+    {
+        switch(unit)
+        {
+            case CELSIUS:
+                return temp * (9/5) + 32;
+            case KELVIN:
+                return temp * (9/5) - 459.67;
+            default:
+                return temp;
+        }
+    }
+
+    private double toKelvin(double temp, Unit unit)
+    {
+        switch(unit)
+        {
+            case FAHRENHEIT:
+                return (temp + 459.67) * (5/9);
+            case CELSIUS:
+                return temp + 273.15;
+            default:
+                return temp;
+        }
+    }
+
+    private Unit getUnit(Spinner aSpinner)
+    {
+        String item = aSpinner.getSelectedItem().toString();
+        switch(item)
+        {
+            case "C° Celsius":
+                return Unit.CELSIUS;
+            case "F° Fahrenheit":
+                return Unit.FAHRENHEIT;
+            default:
+                return Unit.KELVIN;
+
+        }
     }
 }
